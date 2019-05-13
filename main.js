a1dbba442c3ae92a75ada9ec57beb0fb030390ef
Adrianna Rochmalska <ro.adriannaa@gmail.com>
2019-05-13 22:18:25 +0200

    change input / safari probl

--- main.js
+++ main.js
@@ -1,42 +1,47 @@
 // console.log('ok');
 
-const endTime = Math.floor(new Date('2019-06-22 17:00:00')/1000)
+// const endTime = Math.floor(new Date('2019-06-22 17:00:00')/1000)
 
-const finalDate = document.querySelector('.final')
+const finalDate = document.querySelector('.final-date')
+const finalTime = document.querySelector('.final-time')
+const final = `${finalDate.value} ${finalTime.value}`
+
 const btn = document.querySelector('button')
 const spanD = document.querySelector('span.d')
 const spanH = document.querySelector('span.h')
 const spanM = document.querySelector('span.m')
 const spanS = document.querySelector('span.s')
-const newEndTime = Math.floor(new Date(finalDate.value).getTime()/1000)
+const newEndTime = Math.floor(new Date(final).getTime()/1000)
 
+// function count() {
+//     const nowTime = Math.floor(new Date().getTime() / 1000);
+//     const time = newEndTime - nowTime;
 
-function count() {
-    const nowTime = Math.floor(new Date().getTime() / 1000);
-    const time = newEndTime - nowTime;
+//     let days = Math.floor(time/(60*60*24));
+//     days = days<10 ? `0${days}` : days
+//     let hours = Math.floor(time/(60*60)%24);
+//     hours = hours<10 ? `0${hours}` : hours
+//     let minutes = Math.floor(time/(60)%60);
+//     minutes = minutes<10 ? `0${minutes}` : minutes
+//     let secs = Math.floor(time%60);
+//     secs = secs<10 ? `0${secs}` : secs
 
-    let days = Math.floor(time/(60*60*24));
-    days = days<10 ? `0${days}` : days
-    let hours = Math.floor(time/(60*60)%24);
-    hours = hours<10 ? `0${hours}` : hours
-    let minutes = Math.floor(time/(60)%60);
-    minutes = minutes<10 ? `0${minutes}` : minutes
-    let secs = Math.floor(time%60);
-    secs = secs<10 ? `0${secs}` : secs
+//     spanD.textContent = days
+//     spanH.textContent = hours
+//     spanM.textContent = minutes
+//     spanS.textContent = secs
+// }
 
-    spanD.textContent = days
-    spanH.textContent = hours
-    spanM.textContent = minutes
-    spanS.textContent = secs
-}
-
-let interval = setInterval(count, 1000)
+// let interval = setInterval(count, 1000)
 
 function newCount() {
-    clearInterval(interval)
-    const newEndTime = Math.floor(new Date(finalDate.value).getTime()/1000)
+    if(Math.floor(new Date(finalDate.value).getTime()/1000) < Math.floor(new Date().getTime() / 1000)) {
+        return alert(`You can't change the past...`)
+    } else {
+        // clearInterval(interval)
+        const newEndTime = Math.floor(new Date(finalDate.value).getTime()/1000)
 
-     interval = setInterval(()=>{
+        interval = setInterval(()=>{
         const nowTime = Math.floor(new Date().getTime() / 1000);
         const time = newEndTime - nowTime;
     
@@ -54,8 +59,8 @@ function newCount() {
         spanM.textContent = minutes
         spanS.textContent = secs
     
-    }, 1000)
-
+      }, 1000)
+    }
 }
 
 btn.addEventListener('click', newCount)
